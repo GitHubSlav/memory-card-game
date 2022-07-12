21a1acbf7e269d52e66bd4fcb0ba200e85ff36c7
GitHubSlav <githubslav@gmail.com>
2022-07-12 19:24:02 +0700

    Added card-flipping logic

--- src/app/memoryGame/memoryGame.component.ts
+++ src/app/memoryGame/memoryGame.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Card } from './../../utilities/card';
 
 @Component ({
     selector : "memory-game",
@@ -15,20 +16,36 @@ export class MemoryGameComponent {
         "bi bi-bandaid",
         "bi bi-bank"
     ]
-    cards : string[] = [];
+    cards : Card[];
     numOfCards : number = 12;
+    numFlipped : number = 0;
     
     constructor(){
+        this.cards = Array<Card>(this.numOfCards);
         for (let i = 0; i < this.numOfCards; i += 2){
-            this.cards.push(this.images[i/2]);
-            this.cards.push(this.images[i/2]);
+            for (let twice = 0; twice < 2; twice++){
+                let randIdx = Math.floor(Math.random() * this.numOfCards);
+
+                while(this.cards[randIdx] !== undefined){
+                    randIdx = (randIdx + 1) % this.numOfCards;
+                }
+
+                this.cards[randIdx] = new Card(randIdx, this.images[i/2]);
+            }
         }
+    }
 
-        for (let i = 0; i < this.numOfCards * 2; i++){
-            let idx_1 = Math.floor(Math.random() * this.numOfCards);
-            let idx_2 = Math.floor(Math.random() * this.numOfCards);
-
-            [this.cards[idx_1], this.cards[idx_2]] = [this.cards[idx_2], this.cards[idx_1]];
+    onFlip(e : number){
+        if (!this.cards[e].isFlipped && this.numFlipped < 2){
+            this.cards[e].Flip();
+            if (++this.numFlipped == 2){
+                setTimeout (() => {
+                    this.numFlipped = 0;
+                    this.cards.forEach(element => {
+                    if (element.isFlipped) element.Flip();
+                });
+                }, 1000);
+            }
         }
     }
 }
