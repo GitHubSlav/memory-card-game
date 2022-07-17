ef3c8220d7a21444a3038f53094a928fe2674be0
GitHubSlav <githubslav@gmail.com>
2022-07-17 14:27:13 +0700

    Auto-generated commit [ci skip]

--- src/main.ts
+++
@@ -1,5 +0,0 @@
-import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
-import { MemoryGameModule } from './app/memoryGame/memoryGame.module';
-
-const platfrom = platformBrowserDynamic();
-platfrom.bootstrapModule(MemoryGameModule);
